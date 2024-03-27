f0aa0ba6a076b0864623ddf1db0f1d5f58dac600
Awajsharma12 <awajsharma12@gmail.com>
2024-03-27 14:21:51 +0545

    Vue Basic

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,70 @@
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up </a>
+        <a href="#">login </a>
+      </template>
+      <h1>vue Giveaway</h1>
+      <p>use code vue for discount</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Pubg UC Giveaway</h1>
+      <p>use code PUB for discount</p>
+    </Modal>
+  </teleport>
+
+  <button @click.alt="toggleModal">Open Modal (alt)</button>
+
+  <button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: { Modal},
+  data() {
+    return {
+      title: 'My first Vue app:',
+      header: 'Sign Up for Giveaway',
+      text: 'sign up within 20min to get discount',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
+    }
+
+  }
   
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
